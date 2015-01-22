96629d1f4add3ff43b1fa2fd8f09ba31a07c1d3c
Bennett Hoffman <bennett@buttercoin.com>
2015-01-22 10:46:00 -0800

    Setup watching for changed tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
@@ -16,15 +16,34 @@ module.exports = function(grunt) {
         }
       }
     },
-    simplemocha: {
-      dev:{
-        src: 'test/*.coffee',
+    mochaTest: {
+      test: {
         options: {
           reporter: 'spec',
-          slow: 200,
-          timeout: 1000
-        }
+          clearRequireCache: true
+        },
+        src: ['test/*.coffee']
+      }
+    },
+
+    watch: {
+      js: {
+        options: {
+          spawn: false,
+        },
+        files: '**/*.coffee',
+        tasks: ['default']
       }
     }
   });
+
+  var defaultTestSrc = grunt.config('mochaTest.test.src');
+  grunt.event.on('watch', function(action, filepath) {
+    grunt.config('mochaTest.test.src', defaultTestSrc);
+    if (filepath.match('test/')) {
+      grunt.config('mochaTest.test.src', filepath);
+    }
+  });
+
+  grunt.registerTask('default', 'mochaTest');
 };
